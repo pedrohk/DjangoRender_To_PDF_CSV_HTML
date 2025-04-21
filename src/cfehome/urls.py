ec0f43f1f3f865669a6b98526590cd9a39626adc
Pedro <pedrohk@gmail.com>
2025-04-20 23:28:54 -0300

    correção
    Testando correção

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("html/", views.invoice_view_html, name="htmlpage"),
-    path("pdf/", views.invoice_view_pdf, name="pdfpage"),
-    path("csv/", views.invoice_view_csv, name="csvpage"),
-    path("admin/", admin.site.urls),    
+    path('html/', views.invoice_view_html, name='htmlpage'),
+    path('pdf/', views.invoice_view_pdf, name='pdfpage'),
+    path('csv/', views.invoice_view_csv, name='csvpage'),
+    path('admin/', admin.site.urls)
 ]
